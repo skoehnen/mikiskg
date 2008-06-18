942e00f14ccca6f1c6ecff13c00c94f947a666a4
ralph.guelpers <ralph.guelpers@3c9242a5-ef4f-0410-acc4-2f34c8133b71>
2008-06-18 13:39:06 +0000

    DB Classe erzeugt mit 4 MEthoden

---
+++ trunk/PrototypMIS/DB_Verarbeitung.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlServerCe;
+
+namespace PrototypMIS
+{
+    class DB_Verarbeitung
+    {
+        private SqlCeConnection DBVerbindung()
+        {
+            return conn = new SqlCeConnection("Data Source = //AppDatabase1.sdf");
+        }
+
+        public Boolean verknuepfung_eintragen(ItemId QuellID, ItemId ZielID)
+        {
+            SqlCeConnection conn = DBVerbindung();
+            conn.Open();
+            SqlCeCommand sqlcommand = conn.CreateCommand();
+            sqlcommand.CommandText = "Insert into Verknüpfung (ItemID_Quelle,ItemID_Senke) VALUES ('" + QuellID + "','" + ZielID + "')";
+            sqlcommand.ExecuteNonQuery();
+            conn.close();
+        }
+
+        public Boolean einzelverknuepfung_loeschen(ItemId Quelle, ItemId Ziel)
+        {
+            SqlCeConnection conn = DBVerbindung();
+            conn.Open();
+            SqlCeCommand sqlcommand = conn.CreateCommand();
+            sqlcommand.CommandText = "Delete from Verknüpfung where ItemID_Quelle = '" + Quelle + "' AND ItemId_Senke = '" + Ziel + "'" ;
+            sqlcommand.ExecuteNonQuery();
+            conn.close();
+
+        }
+
+        public Boolean gesamtverknuepfung_loeschen(ItemId Quelle, ItemId Ziel)
+        {
+            SqlCeConnection conn = DBVerbindung();
+            conn.Open();
+            SqlCeCommand sqlcommand = conn.CreateCommand();
+            sqlcommand.CommandText = "Delete from Verknüpfung where ItemID_Quelle = '" + Quelle + "'";
+            sqlcommand.ExecuteNonQuery();
+            conn.close();
+
+        }
+
+        public void abfrage(ItemId Quelle)
+        {
+            SqlCeConnection conn = DBVerbindung();
+            conn.Open();
+            SqlCeCommand sqlcommand = conn.CreateCommand();
+            sqlcommand.CommandText = "SELECT * from Verknüpfung where ItemID_Quelle = '" + Quelle + "'";
+            SqlCeDataReader ResultSet;
+            ResultSet = sqlcommand.ExecuteReader;
+            
+            conn.close();
+        }
+    }
+}
