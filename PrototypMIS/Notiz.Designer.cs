1ee1e3d2a11e3c90570800caea3ce569033bde64
stefan.khnen <stefan.khnen@3c9242a5-ef4f-0410-acc4-2f34c8133b71>
2008-06-19 12:43:46 +0000

    Form für Notiz erzeugt (Noch nicht mit Funktionalität). Menü des Notizen Form so erweitert das man die neue Form aufrufen kann.

---
+++ PrototypMIS/Notiz.Designer.cs
@@ -0,0 +1,42 @@
+﻿namespace PrototypMIS
+{
+    partial class Notiz
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.Menu = this.mainMenu1;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.Text = "Notiz";
+            this.AutoScroll = true;
+        }
+
+        #endregion
+    }
+}
