cdbf3795d03293658a01b212180078dcf412021e
stefan.khnen <stefan.khnen@3c9242a5-ef4f-0410-acc4-2f34c8133b71>
2008-07-01 09:56:50 +0000

    Notizen fertig

--- PrototypMIS/Notizen.Designer.cs
+++ PrototypMIS/Notizen.Designer.cs
@@ -33,6 +33,10 @@
             this.menuItemZurueck = new System.Windows.Forms.MenuItem();
             this.menuItemMenu = new System.Windows.Forms.MenuItem();
             this.menuItemCreateNote = new System.Windows.Forms.MenuItem();
+            this.dataGrid1 = new System.Windows.Forms.DataGrid();
+            this.contextMenu1 = new System.Windows.Forms.ContextMenu();
+            this.menuItemShow = new System.Windows.Forms.MenuItem();
+            this.menuItemDelete = new System.Windows.Forms.MenuItem();
             this.SuspendLayout();
             // 
             // mainMenu1
@@ -55,15 +59,40 @@
             this.menuItemCreateNote.Text = "Notiz anlegen";
             this.menuItemCreateNote.Click += new System.EventHandler(this.menuItemCreateNote_Click);
             // 
+            // dataGrid1
+            // 
+            this.dataGrid1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.dataGrid1.ContextMenu = this.contextMenu1;
+            this.dataGrid1.Location = new System.Drawing.Point(0, 3);
+            this.dataGrid1.Name = "dataGrid1";
+            this.dataGrid1.Size = new System.Drawing.Size(240, 262);
+            this.dataGrid1.TabIndex = 0;
+            // 
+            // contextMenu1
+            // 
+            this.contextMenu1.MenuItems.Add(this.menuItemShow);
+            this.contextMenu1.MenuItems.Add(this.menuItemDelete);
+            // 
+            // menuItemShow
+            // 
+            this.menuItemShow.Text = "anzeigen";
+            this.menuItemShow.Click += new System.EventHandler(this.menuItemShow_Click);
+            // 
+            // menuItemDelete
+            // 
+            this.menuItemDelete.Text = "löschen";
+            // 
             // Notizen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.dataGrid1);
             this.Menu = this.mainMenu1;
             this.Name = "Notizen";
             this.Text = "Notizen";
+            this.GotFocus += new System.EventHandler(this.gotFocus);
             this.ResumeLayout(false);
 
         }
@@ -73,5 +102,9 @@
         private System.Windows.Forms.MenuItem menuItemZurueck;
         private System.Windows.Forms.MenuItem menuItemMenu;
         private System.Windows.Forms.MenuItem menuItemCreateNote;
+        private System.Windows.Forms.DataGrid dataGrid1;
+        private System.Windows.Forms.ContextMenu contextMenu1;
+        private System.Windows.Forms.MenuItem menuItemShow;
+        private System.Windows.Forms.MenuItem menuItemDelete;
     }
 }
