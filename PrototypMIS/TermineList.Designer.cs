222c7bd0bcbe2dba2c6fd4c710d29b9af7d5ed94
marc.schmitz.81 <marc.schmitz.81@3c9242a5-ef4f-0410-acc4-2f34c8133b71>
2008-07-21 13:10:25 +0000

    TermineList.cs erstellt und implementiert. Dafür musste Termine.cs gelöscht werden!!!

--- PrototypMIS/TermineList.Designer.cs
+++ PrototypMIS/TermineList.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PrototypMIS
 {
-    partial class KontakteList
+    partial class TermineList
     {
         /// <summary>
         /// Erforderliche Designervariable.
@@ -31,88 +31,87 @@
         {
             this.mainMenu1 = new System.Windows.Forms.MainMenu();
             this.menuItemBack = new System.Windows.Forms.MenuItem();
-            this.menuItem2 = new System.Windows.Forms.MenuItem();
-            this.menuItemCreateTask = new System.Windows.Forms.MenuItem();
+            this.menuItemMenu = new System.Windows.Forms.MenuItem();
             this.menuItemSearch = new System.Windows.Forms.MenuItem();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuItemNewAppointment = new System.Windows.Forms.MenuItem();
+            this.listViewTermine = new System.Windows.Forms.ListView();
             this.contextMenu1 = new System.Windows.Forms.ContextMenu();
-            this.menuItemDelete = new System.Windows.Forms.MenuItem();
-            this.menuItemShowEntry = new System.Windows.Forms.MenuItem();
+            this.menuItemDeleteListItem = new System.Windows.Forms.MenuItem();
+            this.menuItemShowListItem = new System.Windows.Forms.MenuItem();
             this.SuspendLayout();
             // 
             // mainMenu1
             // 
             this.mainMenu1.MenuItems.Add(this.menuItemBack);
-            this.mainMenu1.MenuItems.Add(this.menuItem2);
+            this.mainMenu1.MenuItems.Add(this.menuItemMenu);
             // 
             // menuItemBack
             // 
             this.menuItemBack.Text = "zurück";
             this.menuItemBack.Click += new System.EventHandler(this.menuItemBack_Click);
             // 
-            // menuItem2
+            // menuItemMenu
             // 
-            this.menuItem2.MenuItems.Add(this.menuItemCreateTask);
-            this.menuItem2.MenuItems.Add(this.menuItemSearch);
-            this.menuItem2.Text = "Menü";
-            // 
-            // menuItemCreateTask
-            // 
-            this.menuItemCreateTask.Text = "Aufgabe anlegen";
-            this.menuItemCreateTask.Click += new System.EventHandler(this.menuItemCreateTask_Click);
+            this.menuItemMenu.MenuItems.Add(this.menuItemSearch);
+            this.menuItemMenu.MenuItems.Add(this.menuItemNewAppointment);
+            this.menuItemMenu.Text = "Menü";
             // 
             // menuItemSearch
             // 
             this.menuItemSearch.Text = "Suchen";
             this.menuItemSearch.Click += new System.EventHandler(this.menuItemSearch_Click);
             // 
-            // listView1
+            // menuItemNewAppointment
             // 
-            this.listView1.ContextMenu = this.contextMenu1;
-            this.listView1.Location = new System.Drawing.Point(0, 3);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(240, 262);
-            this.listView1.TabIndex = 0;
-            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.menuItemNewAppointment.Text = "Termin anlegen";
+            this.menuItemNewAppointment.Click += new System.EventHandler(this.menuItemNewAppointment_Click);
+            // 
+            // listViewTermine
+            // 
+            this.listViewTermine.ContextMenu = this.contextMenu1;
+            this.listViewTermine.Location = new System.Drawing.Point(0, 0);
+            this.listViewTermine.Name = "listViewTermine";
+            this.listViewTermine.Size = new System.Drawing.Size(240, 268);
+            this.listViewTermine.TabIndex = 0;
             // 
             // contextMenu1
             // 
-            this.contextMenu1.MenuItems.Add(this.menuItemDelete);
-            this.contextMenu1.MenuItems.Add(this.menuItemShowEntry);
+            this.contextMenu1.MenuItems.Add(this.menuItemDeleteListItem);
+            this.contextMenu1.MenuItems.Add(this.menuItemShowListItem);
             // 
-            // menuItemDelete
+            // menuItemDeleteListItem
             // 
-            this.menuItemDelete.Text = "Löschen";
-            this.menuItemDelete.Click += new System.EventHandler(this.menuItemDelete_Click);
+            this.menuItemDeleteListItem.Text = "Löschen";
+            this.menuItemDeleteListItem.Click += new System.EventHandler(this.menuItemDeleteListItem_Click);
             // 
-            // menuItemShowEntry
+            // menuItemShowListItem
             // 
-            this.menuItemShowEntry.Text = "Anzeigen";
-            this.menuItemShowEntry.Click += new System.EventHandler(this.menuItemShowEntry_Click);
+            this.menuItemShowListItem.Text = "Anzeigen";
+            this.menuItemShowListItem.Click += new System.EventHandler(this.menuItemShowListItem_Click);
             // 
-            // KontakteList
+            // TermineList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(240, 268);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.listViewTermine);
             this.Menu = this.mainMenu1;
-            this.Name = "KontakteList";
-            this.Text = "Aufgaben";
+            this.Name = "TermineList";
+            this.Text = "Termine";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.ListView listViewTermine;
         private System.Windows.Forms.MenuItem menuItemBack;
-        private System.Windows.Forms.MenuItem menuItem2;
-        private System.Windows.Forms.MenuItem menuItemCreateTask;
-        private System.Windows.Forms.MenuItem menuItemSearch;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.MenuItem menuItemMenu;
         private System.Windows.Forms.ContextMenu contextMenu1;
-        private System.Windows.Forms.MenuItem menuItemDelete;
-        private System.Windows.Forms.MenuItem menuItemShowEntry;
+        private System.Windows.Forms.MenuItem menuItemDeleteListItem;
+        private System.Windows.Forms.MenuItem menuItemShowListItem;
+        private System.Windows.Forms.MenuItem menuItemSearch;
+        private System.Windows.Forms.MenuItem menuItemNewAppointment;
     }
 }
---
+++ PrototypMIS/TermineList.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.WindowsMobile.PocketOutlook;
+
+namespace PrototypMIS
+{
+    public partial class TermineList : Form
+    {
+        OutlookCommunication oc = null;
+        AppointmentCollection collection = null;
+
+        public TermineList()
+        {
+            InitializeComponent();
+            this.oc = new OutlookCommunication();
+            this.update();
+        }
+
+        private void menuItemSearch_Click(object sender, EventArgs e)
+        {
+            new Suchen().Show();
+        }
+
+        private void menuItemNewAppointment_Click(object sender, EventArgs e)
+        {
+            new Termin().Show();
+            this.Dispose();
+        }
+
+        private void menuItemShowListItem_Click(object sender, EventArgs e)
+        {
+            int itemIndex = this.listViewTermine.SelectedIndices[0];
+            ListViewItem item = this.listViewTermine.Items[itemIndex];
+            MikiDuo auswahl = (MikiDuo)item.Tag;
+            this.Close();
+            new Termin(new ItemId(auswahl.id)).Show();
+            this.Dispose();
+        }
+
+        private void menuItemDeleteListItem_Click(object sender, EventArgs e)
+        {
+            if (secureDelete.boolDelete())
+            {
+                int itemIndex = this.listViewTermine.SelectedIndices[0];
+                ListViewItem item = this.listViewTermine.Items[itemIndex];
+                MikiDuo auswahl = (MikiDuo)item.Tag;
+                new DB_Verarbeitung().gesamtverknuepfung_loeschen(auswahl.id, Konstanten.termin);
+                this.oc.deleteAppointment(MikiConverter.objectToItemId(auswahl.id));
+                this.update();
+            }
+        }
+
+        private void update()
+        {
+            this.collection = this.oc.getOutlookSession().Appointments.Items;
+
+            // ListView leeren
+            this.listViewTermine.Clear();
+
+            // Spalten erzeugen
+            this.listViewTermine.View = View.Details;
+            this.listViewTermine.FullRowSelect = true;
+            this.listViewTermine.Columns.Add("Beschreibung", -2, HorizontalAlignment.Left);
+            this.listViewTermine.Columns.Add("Beginnt", -2, HorizontalAlignment.Left);
+            this.listViewTermine.Columns.Add("Endet", -2, HorizontalAlignment.Left);
+
+            // Items erzeugen
+            foreach (Appointment app in this.collection)
+            {
+                ListViewItem item = new ListViewItem(app.Subject);
+                item.SubItems.Add(app.Start.Day + "." + app.Start.Month + "." + app.Start.Year);
+                item.SubItems.Add(app.End.Day + "." + app.End.Month + "." + app.End.Year);
+                item.Tag = new MikiDuo(MikiConverter.itemIdToInt(app.ItemId), Konstanten.termin);
+                this.listViewTermine.Items.Add(item);
+            }
+        }
+
+        private void menuItemBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            this.Dispose();
+        }
+    }
+}
